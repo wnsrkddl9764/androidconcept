7ab5e5dc99582187064f0432c0ba05be3972633e
wnsrkddl9764 <wnsrkddl9764@naver.com>
2022-03-20 11:50:39 +0900

    13. 예외처리 추가

--- kortlin/10. 이터러블/실습.kts
+++ kortlin/10. 이터러블/실습.kts
@@ -14,7 +14,7 @@ println(range4)
 val range5 = 10 downTo 1 step 2
 println(range5)
 // Step
-// - 특징 : step에 값가 상관없이 첫번째는 무조건 index 0 부터 시작한다
+// - 특징 : step에 값과 상관없이 첫번째는 무조건 index 0 부터 시작한다
 
 // Collection
 val collection1 = listOf<Int>(1, 2, 3, 4, 5)
